9f7feced7e99952ffa3f4136abf66732780cb45d
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-02 11:57:00 -0700

    lab-13 added tables and db

---
+++ Async-Inn/Models/RoomAmenities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models
+{
+    public class RoomAmenities
+    {
+        public int RoomID { get; set; }
+        public string AmenitiesID { get; set; }
+
+        public Room Room { get; set; }
+        public Amenities Amenities { get; set; }
+    }
+}
