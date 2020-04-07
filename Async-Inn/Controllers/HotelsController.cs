58d94ef60ebfb90bf148fb12d0b988d65f2c46c7
Brandon J <splintercel3000@gmail.com>
2020-04-07 13:26:18 -0700

    Hotel finished started Amenities.

--- Async-Inn/Controllers/HotelsController.cs
+++ Async-Inn/Controllers/HotelsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Async_Inn.Data;
 using Async_Inn.Data.Models;
+using Async_Inn.Models.Interfaces;
 
 namespace Async_Inn.Controllers
 {
@@ -14,25 +15,25 @@ namespace Async_Inn.Controllers
     [ApiController]
     public class HotelsController : ControllerBase
     {
-        private readonly Async_InnDbContext _context;
+        private readonly IHotelManager _hotel;
 
-        public HotelsController(Async_InnDbContext context)
+        public HotelsController(IHotelManager hotel)
         {
-            _context = context;
+            _hotel = hotel;
         }
 
         // GET: api/Hotels
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
         {
-            return await _context.Hotels.ToListAsync();
+            return await _hotel.GetAllHotels();
         }
 
         // GET: api/Hotels/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Hotel>> GetHotel(int id)
         {
-            var hotel = await _context.Hotels.FindAsync(id);
+            var hotel = await _hotel.GetHotelByID(id);
 
             if (hotel == null)
             {
@@ -53,25 +54,10 @@ namespace Async_Inn.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(hotel).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HotelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _hotel.UpdateHotel(id, hotel);
 
             return NoContent();
+
         }
 
         // POST: api/Hotels
@@ -80,31 +66,18 @@ namespace Async_Inn.Controllers
         [HttpPost]
         public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
         {
-            _context.Hotels.Add(hotel);
-            await _context.SaveChangesAsync();
+            var result = await _hotel.CreateHotel(hotel);
 
-            return CreatedAtAction("GetHotel", new { id = hotel.ID }, hotel);
+            return CreatedAtAction("GetHotel", new { id = hotel.ID }, result);
         }
 
         // DELETE: api/Hotels/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Hotel>> DeleteHotel(int id)
         {
-            var hotel = await _context.Hotels.FindAsync(id);
-            if (hotel == null)
-            {
-                return NotFound();
-            }
+            await _hotel.RemoveHotel(id);
 
-            _context.Hotels.Remove(hotel);
-            await _context.SaveChangesAsync();
-
-            return hotel;
-        }
-
-        private bool HotelExists(int id)
-        {
-            return _context.Hotels.Any(e => e.ID == id);
+            return NoContent();
         }
     }
 }
---
+++ Async-Inn/Models/Services/HotelService.cs
@@ -0,0 +1,48 @@
+﻿using Async_Inn.Data;
+using Async_Inn.Data.Models;
+using Async_Inn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models.Services
+{
+    public class HotelService : IHotelManager
+
+    {
+        private Async_InnDbContext _context;
+
+        public HotelService(Async_InnDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Hotel> CreateHotel(Hotel hotel)
+        {
+            _context.Hotels.Add(hotel);
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+
+        public async Task<List<Hotel>> GetAllHotels() => await _context.Hotels.ToListAsync();
+
+        public async Task<Hotel> GetHotelByID(int hotelID) => await _context.Hotels.FindAsync(hotelID);
+
+        public async Task RemoveHotel(int hotelID) 
+        {
+            Hotel hotel = await GetHotelByID(hotelID);
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+        }
+
+    
+
+        public async Task UpdateHotel(int hotelID, Hotel hotel)
+        {
+            _context.Entry(hotel).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
