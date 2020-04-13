18070805531762c299017874e46bb1dbd10b56e5
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-12 23:50:48 -0700

    deployed back end, created front end project with controller, view, models, interface and service for hotel

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Interfaces;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHotelManager _hotel;
+
+        public HomeController(IHotelManager hotel)
+        {
+            _hotel = hotel;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _hotel.GetAllHotels();
+            return View(result);
+        }
+/*        public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApp.Models.Interfaces;
+using WebApp.Models.Services;
 
 namespace WebApp
 {
@@ -16,6 +18,9 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddTransient<IHotelManager, HotelService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +35,8 @@ namespace WebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // sets the default routing of our requests
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
